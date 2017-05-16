7c40b6ce1a8fba218a08fc8bf0929c365db9e51a
Scott Chamberlain <myrmecocystus+r@gmail.com>
2017-05-16 04:34:58 +0000

    version 0.2.0

--- tests/testthat/test-literature_retractions.R
+++ tests/testthat/test-literature_retractions.R
@@ -1,9 +1,10 @@
 context("literature_retractions")
 
 test_that("literature_retractions", {
+	skip_on_cran()
+
   tt <- literature_retractions()
 
   expect_is(tt, "character")
-  expect_equal(length(tt), 4653)
-  expect_match(tt[1], "21386829")
+  expect_gt(length(tt), 4000)
 })
